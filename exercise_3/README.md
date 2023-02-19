c86e73f7f2b7607229cb5dc4fcfe67e1f4e1ec3c
Alexis Auriac <alexis.auriac@epitech.eu>
2023-02-19 14:47:40 +0100

    part 3: implemented 2nd heuristic for kmeans

--- exercise_3/README.md
+++ exercise_3/README.md
@@ -22,4 +22,48 @@ https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_ana
 
 ### Kmeans with Knee
 
+(see kmeans_knee.py)
+
+We use Kmeans (with sklearn) to make clusters and the knee/elbow method (with Kneedle) to determin the best number of clusters.
+
 ![Kmeans with knee](images/kmeans_knee.jpg?raw=true)
+
+The exact knee varies with each run since kmeans initialization is random but using the Kneedle's default sensitivity we usely get a knee at k=6, which is consistent with what we can see when we plot the data.
+
+It also sometimes gives k=5, in this case it merges clusters 3 and 5. On rare occasions it gives k=3 which is very odd.
+
+### Kmeans with silhouette analysis
+
+(see kmeans_silhouhette.py)
+
+We test for k in \[2, 8\].
+
+![Kmeans with silhouette (k=2)](images/kmeans_silhouette(k=2).jpg?raw=true)
+![Kmeans with silhouette (k=3)](images/kmeans_silhouette(k=3).jpg?raw=true)
+![Kmeans with silhouette (k=4)](images/kmeans_silhouette(k=4).jpg?raw=true)
+![Kmeans with silhouette (k=5)](images/kmeans_silhouette(k=5).jpg?raw=true)
+![Kmeans with silhouette (k=6)](images/kmeans_silhouette(k=6).jpg?raw=true)
+![Kmeans with silhouette (k=7)](images/kmeans_silhouette(k=7).jpg?raw=true)
+![Kmeans with silhouette (k=8)](images/kmeans_silhouette(k=8).jpg?raw=true)
+
+Here is the silhouette average for each, sorted from highest to lowest.
+
+k | silhouette average
+--- | ---
+6 | 0.887
+5 | 0.792
+7 | 0.765
+4 | 0.660
+8 | 0.659
+2 | 0.556
+3 | 0.549
+
+k=6 clearly has the highest silhouette average which is a good indicator that it is the best clustering.
+
+In other clustering for 7 and 8 we can see negative values, which likely means that some points have been assigned to the wrong cluster, as they more similar to data points in other clusters.
+
+We can see that clustering for has very wide fluctuations in the size of the silhouette plots.
+
+Although 6 clusters seems to be the best, depending on the situation 2, 4, and 5 might be more adapted.
+
+Compared to the knee method this heuristic requires more effort to code and a better understanding of the method and context to interpret it, but it also allows for more nuance.
---
+++ exercise_3/kmeans_silhouette.py
@@ -0,0 +1,111 @@
+#!/bin/env python3
+
+"""
+This code is largely inspired from this tutorial: https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+Computes and plots a silhouette analysis with Kmeans.
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+X = np.load('data.npy')
+
+MIN_K = 2
+MAX_K = 8
+k_silhouette_list = []
+
+for i, n_clusters in enumerate(range(MIN_K, MAX_K + 1)):
+    ## Compute kmeans clusters and silhouette analysis
+    kmeans = KMeans(n_clusters=n_clusters, n_init="auto")
+    cluster_labels = kmeans.fit_predict(X)
+
+    silhouette_avg = silhouette_score(X, cluster_labels)
+    print(f'For n_clusters = {n_clusters} the average silhouette score is: {silhouette_avg:.3f}')
+    k_silhouette_list.append([n_clusters, silhouette_avg])
+
+    sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+    ## Plot silhouette analysis
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+
+    ax1.set_xlim([-0.1, 1])
+    ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+    y_lower = 10
+    for i in range(n_clusters):
+        # Aggregate the silhouette scores for samples belonging to cluster i, and sort them
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10  # 10 for the 0 samples
+
+    ax1.set_title('Silhouette plot')
+    ax1.set_xlabel('Silhouette coefficient values')
+    ax1.set_ylabel('Cluster label')
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color='red', linestyle='--', label=f'silhouette average ({silhouette_avg:.3f})')
+    ax1.legend()
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1]) # We don't really show negative values
+
+    ## Plot the clustering
+    colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(
+        X[:, 0], X[:, 1], marker=".", s=30, lw=0, alpha=0.7, c=colors, edgecolor="k"
+    )
+
+    centers = kmeans.cluster_centers_
+    # Draw white circles at cluster centers
+    ax2.scatter(
+        centers[:, 0],
+        centers[:, 1],
+        marker="o",
+        c="white",
+        alpha=1,
+        s=200,
+        edgecolor="k",
+    )
+
+    for i, c in enumerate(centers):
+        ax2.scatter(c[0], c[1], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
+
+    ax2.set_title('Clustered data')
+    ax2.set_xlabel('1st feature')
+    ax2.set_ylabel('2nd feature')
+
+    plt.suptitle(
+        f'Silhouette analysis for KMeans clustering with n_clusters = {n_clusters}',
+        fontsize=14,
+        fontweight='bold',
+    )
+
+    plt.savefig(f'images/kmeans_silhouette(k={n_clusters})')
+
+
+df = pd.DataFrame.from_records(k_silhouette_list, columns=['k', 'silhouette average'])
+print(df.sort_values(by='silhouette average', ascending=False).to_string(index=False))
